97df00671de3e398660e6f1e909b111c1ae73ae8
Bhavik Ravani <ravanibhavik@gmail.com>
2023-02-13 10:25:18 -0500

    Completed module

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 		printError(errInvalidArguments)
 	}
 
-	strings.ToUpper(os.Args[1])
+	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
